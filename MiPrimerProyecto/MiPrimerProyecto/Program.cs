9750b11b760b05114253d7dfa1c9d8670e515ccb
Daniel Zelaya <danilezelayavargas@gmail.com>
2024-11-27 07:56:06 -0600

    Calculo de la media armonica

--- MiPrimerProyecto/MiPrimerProyecto/Program.cs
+++ MiPrimerProyecto/MiPrimerProyecto/Program.cs
@@ -9,46 +9,55 @@ namespace miPrimerProyecto
     {
         static void Main(string[] args)
         {
-            // Primera serie definida
-            int[] serie = new int[] { 1, 2, 3, 4, 5 };
-            Console.WriteLine("La media aritmética es: {0}, y la desviación típica es: {1}",
-                Media(serie), DesviacionTipica(serie));
-
-            // Serie ingresada por el usuario
+            // Uso de funciones...
+            // Ejercicio: Encontrar la media aritmética, desviación típica y media armónica de una serie de números.
             Console.Write("Ingrese la serie de números separados por comas: ");
             string input = Console.ReadLine();
             string[] numeros = input.Split(',');
 
-            // Convertimos los números ingresados a un arreglo de enteros
-            int[] serieUsuario = Array.ConvertAll(numeros, int.Parse);
+            // Calculamos los valores necesarios.
+            double mediaAritmetica = Media(numeros);
+            double desviacionTipica = Tipica(numeros);
+            double mediaArmonica = Armonica(numeros);
 
-            Console.WriteLine("La media aritmética es: {0}, y la desviación típica es: {1}",
-                Media(serieUsuario), DesviacionTipica(serieUsuario));
+            // Mostramos los resultados.
+            Console.WriteLine("La media aritmética es: {0}, y la desviación típica es: {1}", mediaAritmetica, desviacionTipica);
+            Console.WriteLine("La media aritmética es: {0}, la desviación típica es: {1}, y la media armónica es: {2}",
+                mediaAritmetica, desviacionTipica, mediaArmonica);
 
             Console.ReadLine();
         }
 
-        static double Media(int[] serie)
+        static double Media(string[] serie)
         {
             double suma = 0;
-            foreach (int num in serie)
+            foreach (string num in serie)
             {
-                suma += num;
+                suma += double.Parse(num);
             }
             return suma / serie.Length;
         }
 
-        static double DesviacionTipica(int[] serie)
+        static double Tipica(string[] serie)
         {
-            double m = Media(serie);
+            double media = Media(serie);
             double sumaCuadrados = 0;
-            foreach (int num in serie)
+            foreach (string num in serie)
             {
-                sumaCuadrados += Math.Pow(num - m, 2);
+                double valor = double.Parse(num);
+                sumaCuadrados += Math.Pow(valor - media, 2);
             }
-            double desviacion = Math.Sqrt(sumaCuadrados / serie.Length);
-            return Math.Round(desviacion, 2);
+            return Math.Round(Math.Sqrt(sumaCuadrados / serie.Length), 2);
+        }
+
+        static double Armonica(string[] serie)
+        {
+            double sumaInversa = 0;
+            foreach (string num in serie)
+            {
+                sumaInversa += 1.0 / double.Parse(num);
+            }
+            return Math.Round(serie.Length / sumaInversa, 2);
         }
     }
 }
-
