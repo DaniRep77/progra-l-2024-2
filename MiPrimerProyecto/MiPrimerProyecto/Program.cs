699cb6add98fba3e1f60a85e7b1150417c8f4fb5
Daniel Zelaya <danilezelayavargas@gmail.com>
2024-11-27 07:52:13 -0600

    Serie de numeros introducidos por el usuario separados por comas, par…

--- MiPrimerProyecto/MiPrimerProyecto/Program.cs
+++ MiPrimerProyecto/MiPrimerProyecto/Program.cs
@@ -3,46 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-namespace MiPrimerProyecto
+namespace miPrimerProyecto
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            // Uso de funciones...
-            Console.WriteLine("La suma de 10+5={0}", suma());
-            // Ejercicio: encontrar la media aritmética y la desviación típica de una serie de números.
-
-            Console.Write("Num 1: ");
-            int num1 = int.Parse(Console.ReadLine());
-
-            Console.Write("Num 2: ");
-            int num2 = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("La suma de {0}+{1}={2}", num1, num2, suma(num1, num2));
-
+            // Primera serie definida
             int[] serie = new int[] { 1, 2, 3, 4, 5 };
             Console.WriteLine("La media aritmética es: {0}, y la desviación típica es: {1}",
-                media(serie), tipica(serie));
+                Media(serie), DesviacionTipica(serie));
+
+            // Serie ingresada por el usuario
+            Console.Write("Ingrese la serie de números separados por comas: ");
+            string input = Console.ReadLine();
+            string[] numeros = input.Split(',');
+
+            // Convertimos los números ingresados a un arreglo de enteros
+            int[] serieUsuario = Array.ConvertAll(numeros, int.Parse);
+
+            Console.WriteLine("La media aritmética es: {0}, y la desviación típica es: {1}",
+                Media(serieUsuario), DesviacionTipica(serieUsuario));
 
             Console.ReadLine();
         }
 
-        // Función para sumar números fijos
-        static int suma()
-        {
-            return 10 + 5;
-        }
-
-        // Función para sumar dos números dados
-        static int suma(int a, int b)
-        {
-            return a + b;
-        }
-
-        // Función para calcular la media aritmética de una serie
-        static double media(int[] serie)
+        static double Media(int[] serie)
         {
             double suma = 0;
             foreach (int num in serie)
@@ -52,23 +38,17 @@ namespace MiPrimerProyecto
             return suma / serie.Length;
         }
 
-        // Función para calcular la desviación típica de una serie
-        static double tipica(int[] serie)
+        static double DesviacionTipica(int[] serie)
         {
-            double m = media(serie);
-            double suma = 0;
-
+            double m = Media(serie);
+            double sumaCuadrados = 0;
             foreach (int num in serie)
             {
-                suma += Math.Pow(num - m, 2);
+                sumaCuadrados += Math.Pow(num - m, 2);
             }
-
-            return Math.Round(Math.Sqrt(suma / serie.Length), 2);
+            double desviacion = Math.Sqrt(sumaCuadrados / serie.Length);
+            return Math.Round(desviacion, 2);
         }
     }
 }
-
-
-
-
 
